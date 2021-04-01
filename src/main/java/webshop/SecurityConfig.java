9de0a46deb05d6efdc8846bb1af06da382115af9
poliphonalj <peteri@t-online.hu>
2021-04-01 14:00:05 +0200

    heroku db

--- src/main/java/webshop/SecurityConfig.java
+++ src/main/java/webshop/SecurityConfig.java
@@ -1,16 +1,81 @@
 package webshop;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-class SecurityConfig  {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors()         //kell az angularral valo osszekapcsolodashoz
+                .and().csrf().disable().
+                authorizeRequests().antMatchers("/authenticate", "/v2/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui.html",
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/webjars/**" ,
+                "/swagger.json").
+                permitAll().anyRequest().authenticated().
+                and().sessionManagement().
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS);       //Spring Sec is not building a session
+         //ask to use the filterchain, works for each request
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    //cross.fdsfdfaf
+    @Bean
+    @Qualifier("cors")
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://farmfalat.herokuapp.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT","PATCH","HEAD","OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
+
+
+
+
+
+        /*
 http.cors()         //kell az angularral valo osszekapcsolodashoz
         .and().csrf().disable().
         authorizeRequests().antMatchers("/authenticate", "/v2/api-docs/**",
@@ -23,6 +88,6 @@ http.cors()         //kell az angularral valo osszekapcsolodashoz
         permitAll().anyRequest();
           //Spring Sec is not building a session
         //http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);     //ask to use the filterchain, works for each request
+*/
 
-    }
-}
+
