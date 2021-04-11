bec7b4de511ba2654d2e680945218fbab85efd39
BuildTools <unconfigured@null.spigotmc.org>
2021-04-11 18:48:05 +0200

    sok-sok tudomany

--- src/main/java/webshop/Model/Order/Order.java
+++ src/main/java/webshop/Model/Order/Order.java
@@ -11,44 +11,25 @@ import java.util.List;
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long ID;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private LocalDateTime orderTime;
     private PaymentType paymentType;
     private boolean status;
     private long deliveryFee;
-    private long product_ID;
-    private String product_Name;
-    private String user_FirstName;
-    private String user_LastName;
-    private String user_Email;
-    private String user_Phone;
-    private String user_Address;
+    private long productID;
+    private String productName;
+    private String userFirstName;
+    private String userLastName;
+    private String userEmail;
+    private String userPhone;
+    private String userAddress;
 
     @ManyToOne
     MyUser myUser;
 
     @OneToMany(mappedBy = "order")
-    private List<Order_Item> ordersItemList;
-
-    public Order() {
-    }
-
-    public Order(LocalDateTime orderTime, PaymentType paymentType, boolean status,
-                 long deliveryFee, long product_ID, String product_Name, String user_FirstName,
-                 String user_LastName, String user_Email, String user_Phone, String user_Address) {
-        this.orderTime = orderTime;
-        this.paymentType = paymentType;
-        this.status = status;
-        this.deliveryFee = deliveryFee;
-        this.product_ID = product_ID;
-        this.product_Name = product_Name;
-        this.user_FirstName = user_FirstName;
-        this.user_LastName = user_LastName;
-        this.user_Email = user_Email;
-        this.user_Phone = user_Phone;
-        this.user_Address = user_Address;
-    }
+    private List<OrderItem> ordersItemList;
 
     public LocalDateTime getOrderTime() {
         return orderTime;
@@ -82,59 +63,84 @@ public class Order {
         this.deliveryFee = deliveryFee;
     }
 
-    public long getProduct_ID() {
-        return product_ID;
+    public long getProductID() {
+        return productID;
     }
 
-    public void setProduct_ID(long product_ID) {
-        this.product_ID = product_ID;
+    public void setProductID(long productID) {
+        this.productID = productID;
     }
 
-    public String getProduct_Name() {
-        return product_Name;
+    public String getProductName() {
+        return productName;
     }
 
-    public void setProduct_Name(String product_Name) {
-        this.product_Name = product_Name;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
 
-    public String getUser_FirstName() {
-        return user_FirstName;
+    public String getUserFirstName() {
+        return userFirstName;
     }
 
-    public void setUser_FirstName(String user_FirstName) {
-        this.user_FirstName = user_FirstName;
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
     }
 
-    public String getUser_LastName() {
-        return user_LastName;
+    public String getUserLastName() {
+        return userLastName;
     }
 
-    public void setUser_LastName(String user_LastName) {
-        this.user_LastName = user_LastName;
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
     }
 
-    public String getUser_Email() {
-        return user_Email;
+    public String getUserEmail() {
+        return userEmail;
     }
 
-    public void setUser_Email(String user_Email) {
-        this.user_Email = user_Email;
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 
-    public String getUser_Phone() {
-        return user_Phone;
+    public String getUserPhone() {
+        return userPhone;
     }
 
-    public void setUser_Phone(String user_Phone) {
-        this.user_Phone = user_Phone;
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
     }
 
-    public String getUser_Address() {
-        return user_Address;
+    public String getUserAddress() {
+        return userAddress;
     }
 
-    public void setUser_Address(String user_Address) {
-        this.user_Address = user_Address;
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public MyUser getMyUser() {
+		return myUser;
+	}
+
+	public void setMyUser(MyUser myUser) {
+		this.myUser = myUser;
+	}
+
+	public List<OrderItem> getOrdersItemList() {
+		return ordersItemList;
+	}
+
+	public void setOrdersItemList(List<OrderItem> ordersItemList) {
+		this.ordersItemList = ordersItemList;
+	}
+    
 }
--- src/main/java/webshop/Services/OrderService.java
+++ src/main/java/webshop/Services/OrderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import webshop.Model.Order.Order;
 import webshop.Repository.OrderRepo;
@@ -15,5 +16,10 @@ public class OrderService {
 	
 	public List<Order> getAll(){
 		return this.orderRepo.findAll();
+	}
+	
+	@Transactional
+	public Order save(Order order) {
+		return orderRepo.save(order);
 	}
 }
