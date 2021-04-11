b77f65ded18447160c8cd06e056b2d29d3d4ac85
BuildTools <unconfigured@null.spigotmc.org>
2021-04-11 18:48:05 +0200

    sok-sok tudomany

--- src/main/java/webshop/Controllers/OrderController.java
+++ src/main/java/webshop/Controllers/OrderController.java
@@ -4,18 +4,28 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import webshop.Model.Order.Order;
 import webshop.Services.OrderService;
 
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
 	@Autowired
 	OrderService service;
-	
-	@GetMapping("/orders/all")
-	public List<Order> getAll(){
+
+	@GetMapping("/all")
+	public List<Order> getAll() {
 		return this.service.getAll();
+	}
+
+	@PostMapping("/save")
+	public Order save(@RequestBody Order order) {
+		order = this.service.save(order);
+		return order;
 	}
 }
