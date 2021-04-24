fc4e735851411d4dfde9620a54f1c8cd167aee1d
poliphonalj <peteri@t-online.hu>
2021-04-24 12:59:37 +0200

    slogan reactivate

--- src/main/java/webshop/Controllers/SloganController.java
+++ src/main/java/webshop/Controllers/SloganController.java
@@ -12,6 +12,7 @@ import webshop.Services.SloganService;
 
 import java.util.HashMap;
 import java.util.List;
+
 @RestController
 public class SloganController {
     private SloganService sloganService;
@@ -21,10 +22,11 @@ public class SloganController {
         this.sloganService = sloganService;
     }
 
+    //ok
     @GetMapping("/slogan/random")   //return a random slogan from the db
-    public ResponseEntity<?> getRandom(){
-        Slogan s=sloganService.getRandomSlogan();
-        if (s!=null) {
+    public ResponseEntity<?> getRandom() {
+        Slogan s = sloganService.getRandomSlogan();
+        if (s != null) {
             HashMap<String, Slogan> hMap = new HashMap<>();
             hMap.put("list", s);
             return ResponseEntity.ok().body(hMap);
@@ -32,8 +34,9 @@ public class SloganController {
         return ResponseEntity.badRequest().body(new FeedbackToFrontend(false));
     }
 
+    //ok
     @PostMapping("/slogan/add") //adding a slogan to thee db
-    public ResponseEntity<?> addSlogan(@RequestBody String slogan ) {
+    public ResponseEntity<?> addSlogan(@RequestBody String slogan) {
         try {
             sloganService.addSlogan(slogan);
             return ResponseEntity.ok(new FeedbackToFrontend(true));
@@ -42,8 +45,9 @@ public class SloganController {
         }
     }
 
+    //ok
     @PostMapping("/slogan/remove/{ID}") //remove a slogan from thee db
-    public ResponseEntity<?> removeSlogan(@PathVariable long ID ) {
+    public ResponseEntity<?> removeSlogan(@PathVariable long ID) {
         try {
             sloganService.removeSlogan(ID);
             return ResponseEntity.ok(new FeedbackToFrontend(true));
@@ -52,9 +56,10 @@ public class SloganController {
         }
     }
 
+    //ok
     @GetMapping("/slogan/list")//if its empty??
-    public ResponseEntity<?> listSlogan( ) {
-       List<Slogan>list=sloganService.listSlogan();
+    public ResponseEntity<?> listSlogan() {
+        List<Slogan> list = sloganService.listSlogan();
         if (!(list.isEmpty())) {
             HashMap<String, List<Slogan>> hMap = new HashMap<>();
             hMap.put("list", list);
@@ -63,15 +68,17 @@ public class SloganController {
         return ResponseEntity.badRequest().body(new FeedbackToFrontend(false));
     }
 
+    //ok
     @PostMapping("/slogan/reactivate/{sloganID}")
-    public ResponseEntity<?> listSlogan(@PathVariable long sloganID ) {
-       // sloganService.reactivate(sloganID);
-
+    public ResponseEntity<?> listSlogan(@PathVariable long sloganID) {
+        try {
+            sloganService.reactivate(sloganID);
             return ResponseEntity.ok().body(new FeedbackToFrontend(true));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new FeedbackToFrontend(false));
         }
-       // return ResponseEntity.badRequest().body(new FeedbackToFrontend(false));
     }
-    //slogan/reactivate/ID
+}
 
 
 
