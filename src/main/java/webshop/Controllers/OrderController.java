35e3cc0d39595b7af3388a47292bf73ea3207c49
botikori <botikori@botikori-System-Product-Name>
2021-04-25 15:08:40 +0200

    Order kezelés alapműveletei kész vannak

--- src/main/java/webshop/Controllers/OrderController.java
+++ src/main/java/webshop/Controllers/OrderController.java
@@ -1,9 +1,15 @@
 package webshop.Controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import webshop.Model.Order.Order;
 import webshop.Services.OrderService;
+import webshop.exception.WebshopException;
 
 @RestController
 @RequestMapping("/orders")
@@ -24,8 +31,28 @@ public class OrderController {
 	}
 
 	@PostMapping("/save")
-	public Order save(@RequestBody Order order) {
+	public Order save(@Valid @RequestBody Order order, BindingResult result) throws WebshopException  {
+		if(result.hasErrors()) {
+			System.out.println("rossz");
+			List<String> errorMessages = new ArrayList<>();
+			result.getFieldErrors().forEach(e -> {
+				errorMessages.add(e.getField() + " " + e.getDefaultMessage());
+			});
+			throw new WebshopException(errorMessages);
+		}
 		order = this.service.save(order);
+		return order;
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		service.delete(id);
+		return "Ok";
+	}
+	
+	@GetMapping("/get/{id}")
+	public Order getOne(@PathVariable("id") Long id) {
+		Order order = service.getOne(id);
 		return order;
 	}
 }
--- src/main/java/webshop/Services/OrderService.java
+++ src/main/java/webshop/Services/OrderService.java
@@ -1,6 +1,7 @@
 package webshop.Services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,16 @@ public class OrderService {
 	@Transactional
 	public Order save(Order order) {
 		return orderRepo.save(order);
+	}
+	
+	@Transactional
+	public void delete(long id) {
+		orderRepo.deleteById(id);
+	}
+	
+	public Order getOne(long id) {
+		Optional<Order> opt = orderRepo.findById(id); 
+		return  opt.isPresent() ? opt.get() : null; 
+				
 	}
 }
