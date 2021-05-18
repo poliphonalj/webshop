d8c7f83b37584e1dedfc1f366fb20d0b662c0b1a
poliphonalj <peteri@t-online.hu>
2021-05-19 01:59:31 +0200

    emaail fixed and sent to two addresses

--- src/main/java/webshop/Services/EmailService.java
+++ src/main/java/webshop/Services/EmailService.java
@@ -17,17 +17,19 @@ public class EmailService {
 
     @Autowired
     public EmailService(JavaMailSender javaMailSender, UserRepo userRepo) {
-        this.userRepo=userRepo;
+        this.userRepo = userRepo;
         this.javaMailSender = javaMailSender;
     }
 
     public void successfulRegistration(String firstname, String username) throws MessagingException {
-          String htmlMsg = "Helló, kedves <strong> "+firstname+"</strong>!<br>Köszönjük, hogy regisztráltál a " +
-                  "<strong>farmfalat.hu</strong> weboldalon! Reméljük sok örömödetleled itt...";
+        String htmlMsg = "Helló, kedves <strong> " + firstname + "</strong>!<br>Köszönjük, hogy regisztráltál a " +
+                "<strong>farmfalat.hu</strong> weboldalon! Reméljük sok örömödetleled itt...";
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
         mimeMessage.setContent(htmlMsg, "text/html"); /** Use this or below line **/
         //helper.setText(htmlMsg, true);
+
+        helper.setTo("zoltanmarai51@gmail.com");//ide jon a username
         helper.setTo("poliphonalj@freemail.hu");//ide jon a username
         helper.setSubject("Sikeres regisztráció");
 
@@ -35,12 +37,12 @@ public class EmailService {
     }
 
     public void newPassword(String password, long userID) throws MessagingException {
-        MyUser myUser=userRepo.findUserByUserID(userID);
-        String username=myUser.getUsername();
-        String firstName=myUser.getFirstName();
+        MyUser myUser = userRepo.findUserByUserID(userID);
+        String username = myUser.getUsername();
+        String firstName = myUser.getFirstName();
 
-        String htmlMsg = " <strong> "+firstName+"</strong>!<br> jelszót módosítottál a " +
-                "<strong>farmfalat.hu</strong> -n. Az új jelszavad: <br>"+password+"</br>";
+        String htmlMsg = " <strong> " + firstName + "</strong>!<br> jelszót módosítottál a " +
+                "<strong>farmfalat.hu</strong> -n. Az új jelszavad: <br>" + password + "</br>";
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
         mimeMessage.setContent(htmlMsg, "text/html"); /** Use this or below line **/
@@ -53,14 +55,14 @@ public class EmailService {
 
     public void forgotPassword(String link, long userID) throws MessagingException {
         System.out.println(userID);
-        MyUser m=userRepo.findUserByUserID(userID);
-        String firstName=m.getFirstName();
-        String username=m.getUsername();
+        MyUser m = userRepo.findUserByUserID(userID);
+        String firstName = m.getFirstName();
+        String username = m.getUsername();
         System.out.println("visszatert");
 
-        String htmlMsg = " <strong> "+firstName+"</strong>!<br> elfelejtette a jelszavat a " +
-                "<strong>farmfalat.hu</strong> -n. Kérjük kattintson a kovetkezo linkre <br>"+
-                "<a href="+link+">elfelejtett jelszo csere</a></br>";
+        String htmlMsg = " <strong> " + firstName + "</strong>!<br> elfelejtette a jelszavat a " +
+                "<strong>farmfalat.hu</strong> -n. Kérjük kattintson a kovetkezo linkre <br>" +
+                "<a href=" + link + ">elfelejtett jelszo csere</a></br>";
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
         mimeMessage.setContent(htmlMsg, "text/html"); /** Use this or below line **/
